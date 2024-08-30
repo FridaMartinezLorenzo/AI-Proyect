36a2358a36b130ec628835a870320525afd48dbd
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-08-30 00:58:13 -0600

    LOF solucionado

--- AI_Proyect/Working/LOF/LOF_MINMAX.py
+++ AI_Proyect/Working/LOF/LOF_MINMAX.py
@@ -9,18 +9,18 @@ test_data = pd.read_csv('../TrainTest/Split/test_MinMax.csv')
 
 # Extract features and labels from the train and test datasets
 X_train = train_data.drop(columns=['Label'])
-y_train = train_data['Label'].values  # Convertir a numpy array
+y_train = train_data['Label'].values
 X_test = test_data.drop(columns=['Label'])
-y_test = test_data['Label'].values  # Convertir a numpy array
+y_test = test_data['Label'].values
 
 # Initialize the Local Outlier Factor model with novelty detection enabled
 lof = LocalOutlierFactor(n_neighbors=35, contamination=0.15, novelty=True)
 
-# Fit the model on the training data
-lof.fit(X_train)
+# Fit the model on the training data using values
+lof.fit(X_train.values, y_train)
 
-# Predict on the test data
-y_pred_test = lof.predict(X_test)
+# Predict on the test data using values
+y_pred_test = lof.predict(X_test.values)
 
 print("Predictions on the test set using Local Outlier Factor:")
 print(y_pred_test)
@@ -32,7 +32,7 @@ print("\nBinary predictions on the test set using Local Outlier Factor:")
 print(y_pred_test_bin)
 
 print("\nActual labels on the test set:")
-print(y_test)  # y_test ya es un arreglo de numpy
+print(y_test)
 
 # Evaluate the model on the test data
 print("Accuracy on the test set using Local Outlier Factor:")
